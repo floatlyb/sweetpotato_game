fbedc391a118f73bb9ba694c826e9e79c721e63c
floatlyb <113573876+floatlyb@users.noreply.github.com>
2023-01-17 14:50:36 +0900

    portfolio
    portfolio

---
+++ js/game.js
@@ -0,0 +1,86 @@
+$(function(){
+   $(document).on('touchstart',function(){});
+   let second=15;
+   let score=0;
+   let timer;
+   let counter;
+   let num=0;
+   let clickable=false;
+   let num2=0;
+
+   $('.time_area>div').addClass('on');
+
+   function randomTarget(){
+      $('.play_area>div').off('click');
+
+      num=Math.floor(Math.random()*7);
+      $('.i'+num).addClass('circle')
+
+      num2=Math.floor(Math.random()*3)+1;
+      $('.circle').append($('<img src="img/g'+num2+'.png" alt="">'));
+      
+      setTimeout(function(){
+         $('.circle').find('img').remove();
+         $('.play_area>div').filter('.circle').removeClass('circle');
+      },600);
+      $('.i'+num).on('click',function(){
+         if(num2==1){
+            score+=100;
+            $('.play>.scores>.score2').text(score);
+            clickable=false;
+         }else if(num2==2){
+            score-=100;
+            $('.play>.scores>.score2').text(score);
+            clickable=false;
+         }else{
+            score+=200;
+            $('.play>.scores>.score2').text(score);
+            clickable=false;
+         }
+      });
+
+   }
+   timer=setInterval(randomTarget,800);
+ 
+   
+
+   function countdown(){
+      second--;
+      $('.time').text(second);
+      if(second==0){
+         clearInterval(counter);
+         clearInterval(timer);
+         $('.play').hide();
+         $('.time_area>div').removeClass('on');
+         if(score>=1200){
+            $('.end').show();
+            $('.end').find('.score2').text(score);   
+         }else{
+            $('.end2').show();
+            $('.end2').find('.score2').text(score);
+         }
+         
+      }
+   }
+   counter=setInterval(countdown,1000);
+   
+   $('.again').on('click',function(){
+      console.log('clk')
+      second=15;
+      score=0;
+      num=0;
+      clickable=false;
+      
+      setTimeout(function(){
+         $('.time_area>div').addClass('on');
+         $('.play').show();
+         $('.play').find('.score2').text(score);
+         $('.play').find('.time').text(second);
+         $('.end').hide();
+         $('.end2').hide();
+         timer=setInterval(randomTarget,800);
+         counter=setInterval(countdown,1000);
+      },60);
+   });
+});
+
